78e217c41640270094b0a2e1053b5bdaa1628ca7
joca081 <tidws082@gmail.com>
2025-02-19 15:50:40 -0300

    new commit.V2

--- script.js
+++ script.js
@@ -6,34 +6,41 @@ const contactsList = document.getElementById('contacts-list');
 const nameInput = document.getElementById('name');
 const phoneInput = document.getElementById('phone');
 
+// função onde aceita somente números no campo de telefone
+phoneInput.addEventListener('input', () => {
+    phoneInput.value = phoneInput.value.replace(/[^0-9]/g, '');
+});
+
+
 // Função para adicionar contato
 addButton.addEventListener('click', () => {
-  const name = nameInput.value.trim();
-  const phone = phoneInput.value.trim();
+    const name = nameInput.value.trim();
+    const phone = phoneInput.value.trim();
 
-  if (name === '' || phone === '') {
+    if (name === '' || phone === '') {
     alert('Por favor, preencha todos os campos!');
     return;
-  }
+}
 
-  const listItem = document.createElement('li');
-  listItem.innerHTML = `
+    const listItem = document.createElement('li');
+    listItem.innerHTML = `
     <span>${phone}</span>
     <span>${name}</span>
-  `;
-  contactsList.appendChild(listItem);
+`;
+contactsList.appendChild(listItem);
 
   // Limpar inputs
-  nameInput.value = '';
-  phoneInput.value = '';
+    nameInput.value = '';
+    phoneInput.value = '';
 });
+
 
 // Função para deletar todos os contatos
 deleteButton.addEventListener('click', () => {
-  contactsList.innerHTML = '';
+    contactsList.innerHTML = '';
 });
 
 // Função para recarregar a página
 refreshButton.addEventListener('click', () => {
-  location.reload();
+    location.reload();
 });
